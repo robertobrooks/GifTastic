728bc3e4fdc9b1f6c53ca8f872f7d17b69b9ca62
Roberto Brooks <rob.gate3@gmail.com>
2017-06-09 14:44:42 -0500

    set initial functionality

--- app.js
+++ app.js
@@ -1,80 +1,73 @@
-  // Initial array of movies
+  // Set the initial array of countries
   var topics = ["United States", "Canada", "Mexico", "Argentina"];
 
-  // displayMovieInfo function re-renders the HTML to display the appropriate content
+  // Add function displayCountryInfo re-renders the HTML to display the appropriate content
   function displayCountryInfo() {
 
     var topics = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=dc6zaTOxFJmzC";
 
-    // Creates AJAX call for the specific movie button being clicked
+  // Add the API key and the required parameters
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&limit=10&rating=y&rating=g&api_key=dc6zaTOxFJmzC";
+
+  // Creates AJAX call for the specific country button being clicked
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
 
-      // Creates a div to hold the movie
-
+  // Creates a div to hold the country
       $('#country-view').html("<div>"+topics+"</div>");
 
-      // Retrieves the Rating Data
-      var rating = $("<p>" + response.data[0].rating + "</p>");
+  // Retrieves the Rating and the image
+      for (var i =0; i < response.data.length; i++) {
+      var rating = $("<p>" + response.data[i].rating + "</p>");
       $('#country-view').append(rating);
-      var stillImage = $('<img src="' + response.data[0].images.downsized_still.url + '" />');
+      var stillImage = $('<img src="' + response.data[i].images.downsized_still.url + '" />');
       $('#country-view').append(stillImage);
+    }
 
-      // Creates an element to have the rating displayed
-      // Displays the rating
-      // Retrieves the release year
-      // Creates an element to hold the release year
-      // Displays the release year
-      // Retrieves the plot
-      // Creates an element to hold the plot
-      // Appends the plot
-      // Creates an element to hold the image
-      // Appends the image
-      // Puts the entire Movie above the previous movies.
     });
 
   }
 
-  // Function for displaying movie data
+  // Function for displaying country data
   function renderButtons() {
 
-    // Deletes the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
+  // Deletes the countries prior to adding new
+
     $("#buttons-view").empty();
-    // Loops through the array of movies
+  // Loops through the array of countries
     for (var i = 0; i < topics.length; i++) {
 
-      // Then dynamicaly generates buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+  // Dinamically buttons for each country in the array
       var a = $("<button class='btn-sm col-s-1'>");
-      // Adds a class of movie to our button
+
+  // Adds a class of country to  button
       a.addClass("country");
-      // Added a data-attribute
+  // Added a data-attribute
       a.attr("data-name", topics[i]);
-      // Provided the initial button text
+  // Provided the initial button text
       a.text(topics[i]);
-      // Added the button to the buttons-view div
+  // Added the button to the buttons-view div
       $("#buttons-view").append(a);
     }
   }
 
-  // This function handles events where the add movie button is clicked
+  // This function handles when add country button is clicked
   $("#add-country").on("click", function(event) {
     event.preventDefault();
-    // This line of code will grab the input from the textbox
+
+  // This line of code will grab the input from the textbox
     var country = $("#country-input").val().trim();
 
-    // The movie from the textbox is then added to our array
+  // The country from the textbox is then added to our array
     topics.push(country);
 
-    // Calling renderButtons which handles the processing of our movie array
+  // Calling renderButtons which handles the processing of our country array
     renderButtons();
   });
 
-  // Adding click event listeners to all elements with a class of "movie"
+  // Adding click event listeners to all elements with a class of "country"
   $(document).on("click", ".country", displayCountryInfo);
 
   // Calling the renderButtons function to display the intial buttons
