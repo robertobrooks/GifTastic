c36db70aaf580c7dbc078cc8cd58e48f21e05a73
Roberto Brooks <rob.gate3@gmail.com>
2017-06-09 20:08:41 -0500

    styled code

--- app.js
+++ app.js
@@ -10,25 +10,25 @@
 
     country = $(this).attr("data-name");
 
-  // Add the API key and the required parameters
+    // Add the API key and the required parameters
     queryURL = "https://api.giphy.com/v1/gifs/search?q=" + country + "&limit=10&rating=y&rating=g&api_key=dc6zaTOxFJmzC";
 
-  // Creates AJAX call for the specific country button being clicked
+    // Creates AJAX call for the specific country button being clicked
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
 
-  // Creates a div to hold the country
-      $('#country-view').html("<div class='h2'>"+country+"</div>");
+      // Creates a div to hold the country
+      $('#country-view').html("<div class='h2'>" + country + "</div>");
 
-  // Retrieves the Rating and the image
-      for (var i =0; i < response.data.length; i++) {
-      var rating = $("<div>Rating: " + response.data[i].rating + "</div>");
-      $('#country-view').append(rating);
-      var stillImage = $('<img id="'+i+'" class="still" src="' + response.data[i].images.downsized_still.url + '" />');
-      $('#country-view').append(stillImage);
-    }
+      // Retrieves the Rating and the image
+      for (var i = 0; i < response.data.length; i++) {
+        var rating = $("<div>Rating: " + response.data[i].rating + "</div>");
+        $('#country-view').append(rating);
+        var stillImage = $('<img id="' + i + '" class="still" src="' + response.data[i].images.downsized_still.url + '" />');
+        $('#country-view').append(stillImage);
+      }
 
     });
 
@@ -37,29 +37,29 @@
   function animateGif() {
 
     imageSelected = $(this).attr("id");
-  // Add the API key and the required parameters
+    // Add the API key and the required parameters
 
-  // Creates AJAX call for the specific country button being clicked
+    // Creates AJAX call for the specific country button being clicked
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
 
-  // Creates a div to hold the country
-      $('#country-view').html("<div class='h2'>"+country+"</div>");
+      // Creates a div to hold the country
+      $('#country-view').html("<div class='h2'>" + country + "</div>");
 
-  // Retrieves the Rating and the image
-      for (var i =0; i < response.data.length; i++) {
-      var rating = $("<div>Rating: " + response.data[i].rating + "</div>");
+      // Retrieves the Rating and the image
+      for (var i = 0; i < response.data.length; i++) {
+        var rating = $("<div>Rating: " + response.data[i].rating + "</div>");
 
-      $('#country-view').append(rating);
-      if (parseInt(imageSelected) === i) {
-      stillImage = $('<img id="'+i+'" class="animated" src="' + response.data[i].images.downsized.url + '" />');
-    } else {
-      stillImage = $('<img id="'+i+'" class="animated" src="' + response.data[i].images.downsized_still.url + '" />');
-    }
-      $('#country-view').append(stillImage);
-    }
+        $('#country-view').append(rating);
+        if (parseInt(imageSelected) === i) {
+          stillImage = $('<img id="' + i + '" class="animated" src="' + response.data[i].images.downsized.url + '" />');
+        } else {
+          stillImage = $('<img id="' + i + '" class="animated" src="' + response.data[i].images.downsized_still.url + '" />');
+        }
+        $('#country-view').append(stillImage);
+      }
 
     });
 
@@ -68,26 +68,26 @@
   function stopGif() {
 
     imageSelected = $(this).attr("id");
-  // Add the API key and the required parameters
+    // Add the API key and the required parameters
 
-  // Creates AJAX call for the specific country button being clicked
+    // Creates AJAX call for the specific country button being clicked
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
 
-  // Creates a div to hold the country
-      $('#country-view').html("<div class='h2'>"+country+"</div>");
+      // Creates a div to hold the country
+      $('#country-view').html("<div class='h2'>" + country + "</div>");
 
-  // Retrieves the Rating and the image
-      for (var i =0; i < response.data.length; i++) {
-      var rating = $("<div>Rating: " + response.data[i].rating + "</div>");
-      $('#country-view').append(rating);
+      // Retrieves the Rating and the image
+      for (var i = 0; i < response.data.length; i++) {
+        var rating = $("<div>Rating: " + response.data[i].rating + "</div>");
+        $('#country-view').append(rating);
 
-      stillImage = $('<img id="'+i+'" class="still" src="' + response.data[i].images.downsized_still.url + '" />');
+        stillImage = $('<img id="' + i + '" class="still" src="' + response.data[i].images.downsized_still.url + '" />');
 
-      $('#country-view').append(stillImage);
-    }
+        $('#country-view').append(stillImage);
+      }
 
     });
 
@@ -97,22 +97,22 @@
   // Function for displaying country data
   function renderButtons() {
 
-  // Deletes the countries prior to adding new
+    // Deletes the countries prior to adding new
 
     $("#buttons-view").empty();
-  // Loops through the array of countries
+    // Loops through the array of countries
     for (var i = 0; i < topics.length; i++) {
 
-  // Dinamically buttons for each country in the array
+      // Dinamically buttons for each country in the array
       var a = $("<button class='btn-sm btn-primary'>");
 
-  // Adds a class of country to  button
+      // Adds a class of country to  button
       a.addClass("country");
-  // Added a data-attribute
+      // Added a data-attribute
       a.attr("data-name", topics[i]);
-  // Provided the initial button text
+      // Provided the initial button text
       a.text(topics[i]);
-  // Added the button to the buttons-view div
+      // Added the button to the buttons-view div
       $("#buttons-view").append(a);
     }
   }
@@ -121,13 +121,13 @@
   $("#add-country").on("click", function(event) {
     event.preventDefault();
 
-  // This line of code will grab the input from the textbox
+    // This line of code will grab the input from the textbox
     var country = $("#country-input").val().trim();
 
-  // The country from the textbox is then added to our array
+    // The country from the textbox is then added to our array
     topics.push(country);
 
-  // Calling renderButtons which handles the processing of our country array
+    // Calling renderButtons which handles the processing of our country array
     renderButtons();
   });
 
